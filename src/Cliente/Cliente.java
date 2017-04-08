83f13906fd3688a585459e325168e71358ac0a6b
Paula Ramirez <pj.ramirez10@uniandes.edu.co>
2017-04-08 14:29:20 -0500

    Cliente y servidor sin threads

--- src/Cliente/Cliente.java
+++ src/Cliente/Cliente.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -22,20 +26,54 @@ public class Cliente {
 		
 	}
 	
-	public static void main(String args[])
+	public static void main(String args[]) throws IOException
 	{
-		try {
-			socket = new Socket(IPThis,9000);
-			
-			PrintWriter out = new PrintWriter(socket.getOutputStream());
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-			System.out.println("La ip local esta erronea");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		System.out.println("/**********************************************************/\n "
+				+ "UDP CLIENT INITIALIZING \n /**********************************************************/");
+		
+		//Crear el host name del server
+		String severHostName = "localhost";
+		
+		//Crear la direccion del servidor
+		InetAddress serverIPAdress = null;
+		
+		//Crear el numero del puerto
+		int serverPortNumber = 9000;
+		
+		//Crear el socket del cliente que se va a usar para conectarse con el servidor de UDP
+		DatagramSocket clientSocket = new DatagramSocket();
+		
+		//Encontrar la direccion IP del servidor con el host name
+		serverIPAdress = InetAddress.getByName(severHostName);
+		
+		System.out.println("Conectandose al servidor con direccion IP " + serverIPAdress 
+				+ " por el puerto UDP " + serverPortNumber + "\n");
+		
+		//Enviar el objeto al servidor
+		String nombre = "Arci";
+		
+		//Preparar el mensaje que se va a enviar
+		String message = "Hola soy " + nombre + "desde el host" + InetAddress.getLocalHost();
+		
+		//Preparar el buffer para enviar el mensaje
+		byte[] sendData = new byte[message.length()*8];
+		sendData = message.getBytes();
+		
+		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverIPAdress, serverPortNumber);
+		clientSocket.send(sendPacket);
+		
+//		try {
+//			socket = new Socket(IPThis,9000);
+//			
+//			PrintWriter out = new PrintWriter(socket.getOutputStream());
+//			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//			
+//		} catch (UnknownHostException e) {
+//			e.printStackTrace();
+//			System.out.println("La ip local esta erronea");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 	}
 
 }
--- src/Servidor/Servidor.java
+++ src/Servidor/Servidor.java
@@ -1,5 +1,56 @@
 package Servidor;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+
 public class Servidor {
 
+	public static void main(String args[])throws IOException
+	{
+		System.out.println("/**********************************************************/\n "
+				+ "UDP SERVER INITIALIZING \n /**********************************************************/");
+		
+		//Declaracion del socket UDP
+		DatagramSocket serverSocket = null;
+		int port;
+		port = 9000;
+		
+		//Creacion del socket UDP con puerto especifico
+		serverSocket = new DatagramSocket(port);
+		System.out.println("Creacion del socket del servidor en el puerto " + port);
+		
+		//Preparar la estructuda de los paquetes recibidos
+		int dataLength = 100;
+		byte[] recievedData = new byte[dataLength]; //buffer del servidor
+		DatagramPacket recievePacket = new DatagramPacket(recievedData, recievedData.length);
+		
+		while(true)
+		{
+			//Esperar por los paquetes entrantes 
+			System.out.println("Esperando por la llegada de nuevos mensajes por el puerto " + port +"...");
+			serverSocket.receive(recievePacket);
+			
+			//Extraer los datos del paquete recibido
+			InetAddress IPAdress = recievePacket.getAddress();	
+			int clientPort = recievePacket.getPort();
+			String message = new String(recievePacket.getData());
+			
+			//Se debe recortar el mensaje con el fin de quitarle los bytes vacios 
+			message = message.trim();
+			System.out.println("[UDP SERVER] El mensaje {" + message + "}. \n ");
+			
+			//enviar la respuesta al cliente
+			byte[] sendData =new byte[1024];
+			String responseMessage = "Mensaje del cliente recibido";
+			sendData = responseMessage.getBytes();
+			
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAdress, clientPort);
+			serverSocket.send(sendPacket);
+			
+		}
+		
+	}
 }
